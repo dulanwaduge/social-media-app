eb9fc340f59905c63b7378d645d3eec2d3b9d95e
Dulan Waduge <dulanwaduge@Dulans-MacBook-Air.local>
2023-01-16 18:28:34 +1100

    created controller for user for crud operations

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,3 +10,7 @@ const router = express.Router();
 
 /* Read */
 router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* Update */
+router.patch("/:id/:friendId", verifyToken);
